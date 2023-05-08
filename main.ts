c5493a38ad441dec1dff8e1cd32b018efe258fc1
Dmitry Shabat <mitya.shabat@gmail.com>
2023-05-08 15:22:36 +0400

    Remove redundant code

--- main.ts
+++ main.ts
@@ -15,21 +15,12 @@ export default class SyncGraphPlugin extends Plugin {
 
 		// this.app.vault.getAbstractFileByPath('.obsidian/graph.json') would return null
 		// So we're doing it the less safe way
-		// const graphConfigJson = await this.app.vault.adapter.read(graphConfigPath);
-		// const graphConfigFile = app.vault.getAbstractFileByPath(graphConfigPath);
-		// if (graphConfigFile instanceof TFile) {
-		if (true) {
-			// const graphConfigJson = await app.vault.read(graphConfigFile);
-			const graphConfigJson = await this.app.vault.adapter.read(graphConfigPath);
-			const graphConfig = JSON.parse(graphConfigJson);
-			const graphColorGroups = graphConfig.colorGroups;
-			this.getLocalGraphLeaves().forEach((leaf) => {
-				this.setColorGroups(leaf, graphColorGroups);
-			})
-		} else {
-			// console.log(graphConfigPath);
-			// console.log(graphConfigFile);
-		}
+		const graphConfigJson = await this.app.vault.adapter.read(graphConfigPath);
+		const graphConfig = JSON.parse(graphConfigJson);
+		const graphColorGroups = graphConfig.colorGroups;
+		this.getLocalGraphLeaves().forEach((leaf) => {
+			this.setColorGroups(leaf, graphColorGroups);
+		})
 	}
 
 	getLocalGraphLeaves() {
