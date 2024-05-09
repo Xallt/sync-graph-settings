82af96795cf03dcc2b75c0ba7d274e0b1aa7b57d
Dmitry <mitya.shabat@gmail.com>
2024-05-09 14:23:41 +0400

    Add a default depth setting to also set the local graph depth on sync

--- main.ts
+++ main.ts
@@ -1,12 +1,58 @@
-import { Plugin, WorkspaceLeaf, normalizePath} from 'obsidian';
+import { Plugin, WorkspaceLeaf, normalizePath, PluginSettingTab , Setting} from 'obsidian';
+
+interface SyncGraphPluginSettings {
+	defaultDepth: number;
+}
+
+const DEFAULT_SETTINGS: SyncGraphPluginSettings = {
+	defaultDepth: 1
+}
+
+export class SyncGraphSettingTab extends PluginSettingTab {
+  plugin: SyncGraphPlugin;
+
+  constructor(app: App, plugin: SyncGraphPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    let { containerEl } = this;
+
+    containerEl.empty();
+
+    new Setting(containerEl)
+      .setName("Default depth")
+      .addText((text) =>
+        text
+          .setPlaceholder("1")
+          .setValue((this.plugin.settings.defaultDepth || 1).toString())
+          .onChange(async (value) => {
+            this.plugin.settings.defaultDepth = Number.parseInt(value) || 1;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
+}
+
 
 export default class SyncGraphPlugin extends Plugin {
+	settings: SyncGraphPluginSettings;
+
 	async onload() {
+		await this.loadSettings();
+
+		this.addSettingTab(new SyncGraphSettingTab(this.app, this));
+
 		this.addCommand({
-			id: "sync-graph-colorgroups-to-localgraph",
+			id: "sync-graph-settings-to-localgraph",
 			name: "Sync Graph Settings to Local Graph",
 			callback: async () => { await this.syncGlobalToLocal() }
 		})
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async syncGlobalToLocal() {
@@ -32,6 +78,11 @@ export default class SyncGraphPlugin extends Plugin {
 		const viewState = localGraphLeaf.getViewState();
 		viewState.state.options.colorGroups = colorGroups;
 		viewState.state.options.search = searchFilters;
+		viewState.state.options.localJumps = this.settings.defaultDepth;
 		localGraphLeaf.setViewState(viewState);
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 }
